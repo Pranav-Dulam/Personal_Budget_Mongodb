713dfce199bde3bd515b5cd4bc607c7cac0e78aa
cowboy-bebop7 <113116323+cowboy-bebop7@users.noreply.github.com>
2025-10-18 11:31:50 -0400

    MongoDB + Mongoose integration with GET and POST endpoints

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
